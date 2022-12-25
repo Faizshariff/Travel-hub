88cb86f88dd49b8dd057f5da4783cb20770cf76f
Faiz <faizshariff540@gmail.com>
2022-12-25 14:22:29 +0530

    finshhim

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
-    './public/components/**/*.tsx',
+    './public/**/*.{js,ts,jsx,tsx}',
   ],
    plugins: [
     require('tailwind-scrollbar'),
